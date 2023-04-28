8915e794441db782b81742fa34886d6f79f0e2ff
Soyeon Park <spark720@gatech.edu>
2023-04-28 10:53:41 -0400

    x

--- env.zsh
+++ env.zsh
@@ -44,6 +44,9 @@ export PATH=/home/soyeon/vuln-genome/tools/retdec/retdec/bin:/home/soyeon/.pyenv
 
 export PATH=$HOME/.cargo/bin:$PATH
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # for skyfire
 HOSTNAME=$(hostname)
